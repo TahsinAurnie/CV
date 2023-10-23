460bdbd0be1353793fa5375160ba29bbd622a5cf
Tahsin Fateha Aurnie <tfaurnie99@gmail.com>
2023-10-23 10:36:32 +0600

    initial views

--- portfolio/main/urls.py
+++ portfolio/main/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from .views import *
 
-app_name = 'main'
+# app_name = 'main'
 urlpatterns =[
     path('', home, name='home'),
-    path('', portfolio, name='portfolio'),
+    # path('', portfolio, name='portfolio'),
     # path('blog/', views.BlogView.as_view(), name='blogs'),
     # path('blog/<slug:slug>', views.BlogDetailView.as_view(), name='blog'),
     # path('contact/', views.IndexView.as_view(), name='main'),
