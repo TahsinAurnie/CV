fe4ec74b6716d92f63562894c1ae50b611f40899
Tahsin Fateha Aurnie <tfaurnie99@gmail.com>
2023-10-23 12:32:02 +0600

    updated

--- portfolio/main/urls.py
+++ portfolio/main/urls.py
@@ -4,10 +4,6 @@ from .views import *
 # app_name = 'main'
 urlpatterns =[
     path('', home, name='home'),
-    # path('', portfolio, name='portfolio'),
-    # path('blog/', views.BlogView.as_view(), name='blogs'),
-    # path('blog/<slug:slug>', views.BlogDetailView.as_view(), name='blog'),
-    # path('contact/', views.IndexView.as_view(), name='main'),
-    # path('', views.IndexView.as_view(), name='main'),
-    # path('', views.IndexView.as_view(), name='main'),
+    path('about/', about, name='about'),
+    path('portfolio/', portfolio, name='portfolio'),
 ]
--- portfolio/main/views.py
+++ portfolio/main/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'header_home.html')
-# def portfolio(request):
-#     return render(request, 'portfolio.html')
+    return render(request, 'home.html')
+def about(request):
+    return render(request, 'about.html')
+def portfolio(request):
+    return render(request, 'portfolio.html')
